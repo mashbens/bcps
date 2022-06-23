639fbb39bab586a044a99014ea8aa55fdc16e0e2
Arif Budiman <arifbdmn69@gmail.com>
2022-06-23 20:54:27 +0700

    docs: updae payment response

--- api/v1/payment/resp/payment_resp.go
+++ api/v1/payment/resp/payment_resp.go
@@ -1,21 +1,40 @@
 package resp
 
-import "github.com/mashbens/cps/business/payment/entity"
+import (
+	payment "github.com/mashbens/cps/business/payment/entity"
+	// member "github.com/mashbens/cps/business/member/entity"
+	// user "github.com/mashbens/cps/business/user/entity"
+
+	member "github.com/mashbens/cps/api/v1/member/resp"
+	user "github.com/mashbens/cps/api/v1/user/resp"
+)
 
 type PaymentResp struct {
-	ID          int    `json:"id"`
-	UserID      int    `json:"user_id"`
-	MembeshipID int    `json:"membership_id"`
-	Amount      int    `json:"amount"`
-	SnapURL     string `json:"snap_url,omitempty"`
+	ID      int               `json:"id"`
+	User    user.UserResp     `json:"user"`
+	Member  member.MemberResp `json:"membership"`
+	Amount  int               `json:"amount"`
+	SnapURL string            `json:"snap_url,omitempty"`
 }
 
-func FromService(payment entity.Payment) PaymentResp {
+func FromService(payment payment.Payment) PaymentResp {
 	return PaymentResp{
-		ID:          int(payment.ID),
-		UserID:      payment.UserID,
-		MembeshipID: payment.MembershipID,
-		Amount:      payment.Amount,
-		SnapURL:     payment.SnapURL,
+		ID: int(payment.ID),
+		User: user.UserResp{
+			ID:             payment.User.ID,
+			Name:           payment.User.Name,
+			Email:          payment.User.Email,
+			Phone:          payment.User.Phone,
+			Member_expired: payment.User.Member_expired,
+			Member_type:    payment.User.Member_type,
+		},
+		Member: member.MemberResp{
+			ID:       payment.Membership.ID,
+			Type:     payment.Membership.Type,
+			Price:    payment.Membership.Price,
+			Duration: payment.Membership.Duration,
+		},
+		Amount:  payment.Amount,
+		SnapURL: payment.SnapURL,
 	}
 }
