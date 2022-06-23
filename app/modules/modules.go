0b9a02cfda59ff7e85e0f2ce7511153faa38e411
Arif Budiman <arifbdmn69@gmail.com>
2022-06-23 18:06:32 +0700

    docs: update modules

--- app/modules/modules.go
+++ app/modules/modules.go
@@ -3,14 +3,22 @@ package modules
 import (
 	"github.com/mashbens/cps/api"
 	"github.com/mashbens/cps/api/v1/auth"
+	"github.com/mashbens/cps/api/v1/payment"
 	"github.com/mashbens/cps/api/v1/user"
 	"github.com/mashbens/cps/config"
 	"github.com/mashbens/cps/util"
 
 	authService "github.com/mashbens/cps/business/user"
 	jwtService "github.com/mashbens/cps/business/user"
+
 	userService "github.com/mashbens/cps/business/user"
 	userRepo "github.com/mashbens/cps/repository/user"
+
+	memberService "github.com/mashbens/cps/business/member"
+	memberRepo "github.com/mashbens/cps/repository/member"
+
+	paymentService "github.com/mashbens/cps/business/payment"
+	paymentRepo "github.com/mashbens/cps/repository/payment"
 )
 
 func RegisterModules(dbCon *util.DatabaseConnection, config *config.AppConfig) api.Controller {
@@ -20,9 +28,16 @@ func RegisterModules(dbCon *util.DatabaseConnection, config *config.AppConfig) a
 	jwtService := jwtService.NewJWTService()
 	authService := authService.NewAuthService(userService, jwtService)
 
+	memberRepo := memberRepo.MemberRepoFactory(dbCon)
+	memberService := memberService.NewMemberService(memberRepo)
+
+	paymentRepo := paymentRepo.PaymentRepositoryFactory(dbCon)
+	paymentService := paymentService.NewPaymentService(paymentRepo, memberService, userService)
+
 	controller := api.Controller{
 		UserAuth: auth.NewAuthController(authService, userService),
 		User:     user.NewUserController(userService, jwtService),
+		Payment:  payment.NewPaymentController(paymentService, jwtService),
 	}
 	return controller
 }
