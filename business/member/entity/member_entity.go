e1bbef5c0f34b5c1985bebac865567e087f588d2
Arif Budiman <arifbdmn69@gmail.com>
2022-06-25 22:29:54 +0700

    docs: add member service

--- business/member/entity/member_entity.go
+++ business/member/entity/member_entity.go
@@ -1,8 +1,14 @@
 package entity
 
+import (
+	superAdmin "github.com/mashbens/cps/business/superadmin/entity"
+)
+
 type Membership struct {
-	ID       int
-	Type     string
-	Price    int
-	Duration int
+	ID            int
+	Type          string
+	Price         int
+	Duration      int
+	Super_adminID int
+	Super_admin   superAdmin.SuperAdmin
 }
--- business/member/service.go
+++ business/member/service.go
@@ -2,25 +2,35 @@ package member
 
 import (
 	"errors"
+	"log"
+	"strconv"
 
 	"github.com/mashbens/cps/business/member/entity"
+	"github.com/mashbens/cps/business/superadmin"
 )
 
 type MemberRepo interface {
 	FindMemberByID(memberID string) (entity.Membership, error)
+	InserMemberships(member entity.Membership) (entity.Membership, error)
 }
 
 type MemberService interface {
 	FindMemberTypeByID(memberID string) (*entity.Membership, error)
+	CreateMemberships(member entity.Membership) (*entity.Membership, error)
 }
 
 type memberService struct {
-	memberRepo MemberRepo
+	memberRepo       MemberRepo
+	superAdminSevice superadmin.SuperAdminService
 }
 
-func NewMemberService(MemberRepo MemberRepo) MemberService {
+func NewMemberService(
+	MemberRepo MemberRepo,
+	superAdminSevice superadmin.SuperAdminService,
+) MemberService {
 	return &memberService{
-		memberRepo: MemberRepo,
+		memberRepo:       MemberRepo,
+		superAdminSevice: superAdminSevice,
 	}
 }
 
@@ -33,4 +43,22 @@ func (c *memberService) FindMemberTypeByID(memberID string) (*entity.Membership,
 
 	return &member, nil
 
+}
+
+func (c *memberService) CreateMemberships(member entity.Membership) (*entity.Membership, error) {
+	strID := strconv.Itoa(member.Super_adminID)
+	sAdmin, err := c.superAdminSevice.FindSuperAdminByID(strID)
+	if err != nil {
+		return nil, err
+	}
+	_ = sAdmin
+	// member.Super_admin = *sAdmin
+
+	log.Println(member)
+	m, err := c.memberRepo.InserMemberships(member)
+	if err != nil {
+		return nil, err
+	}
+
+	return &m, nil
 }
