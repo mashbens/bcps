1df56625520b89d51d8ca074e740e7c535790586
Arif Budiman <arifbdmn69@gmail.com>
2022-06-29 02:48:28 +0700

    docs: update admin  repo

--- repository/admin/record.go
+++ repository/admin/record.go
@@ -5,30 +5,36 @@ import (
 )
 
 type Admin struct {
-	ID       int    `gorm:"primary_key:auto_increment" json:"-"`
-	Name     string `gorm:"type:varchar(100)" json:"-"`
-	Email    string `gorm:"type:varchar(100);unique;" json:"-"`
-	Phone    string `gorm:"type:varchar(100);unique;" json:"-"`
-	Password string `gorm:"type:varchar(100)" json:"-"`
+	ID           int    `gorm:"primary_key:auto_increment" json:"-"`
+	Name         string `gorm:"type:varchar(100)" json:"-"`
+	Email        string `gorm:"type:varchar(100);unique;" json:"-"`
+	Phone        string `gorm:"type:varchar(100);unique;" json:"-"`
+	Password     string `gorm:"type:varchar(100)" json:"-"`
+	SuperAdminID int
+	SuperAdmin   entity.SuperAdmin `gorm:"foreignkey:SuperAdminID" json:"-"`
 }
 
 func (a *Admin) toService() entity.Admin {
 	return entity.Admin{
-		ID:       a.ID,
-		Name:     a.Name,
-		Email:    a.Email,
-		Password: a.Password,
-		Phone:    a.Phone,
+		ID:           a.ID,
+		Name:         a.Name,
+		Email:        a.Email,
+		Password:     a.Password,
+		Phone:        a.Phone,
+		SuperAdminID: a.SuperAdminID,
+		SuperAdmin:   a.SuperAdmin,
 	}
 }
 
 func fromService(a entity.Admin) Admin {
 	return Admin{
-		ID:       a.ID,
-		Name:     a.Name,
-		Email:    a.Email,
-		Password: a.Password,
-		Phone:    a.Phone,
+		ID:           a.ID,
+		Name:         a.Name,
+		Email:        a.Email,
+		Password:     a.Password,
+		Phone:        a.Phone,
+		SuperAdminID: a.SuperAdminID,
+		SuperAdmin:   a.SuperAdmin,
 	}
 }
 func toServiceList(data []Admin) []entity.Admin {
